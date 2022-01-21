04ed83a636e1ed5508ba6bbafefdc78971fc241c
Insignia <olopezmill08@gmail.com>
2022-01-20 23:22:04 -0500

    Erase old code

--- ZipperExtension/source.extension.cs
+++ ZipperExtension/source.extension.cs
@@ -7,11 +7,11 @@ namespace ZipperExtension
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "ZipperExtension.ff9d6561-11c0-4eaa-b5ee-9d602ad3167a";
+        public const string Id = "ZipperExtension.ff9d6561-11c0-4eaa-b5ee-9d603ad3167a";
         public const string Name = "Zipper Extension";
         public const string Description = @"Visual Studio extension to easily zip project files excluding the bin and obj directories.";
         public const string Language = "en-US";
-        public const string Version = "1.0";
+        public const string Version = "1.1";
         public const string Author = "SigDev";
         public const string Tags = "";
     }
